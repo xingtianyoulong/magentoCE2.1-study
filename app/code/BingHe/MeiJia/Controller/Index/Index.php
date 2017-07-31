1faf34af469d8baf81d115d1a228fdcd5c883e76
Bruce Liu <bruceliu@jootop.com>
2017-07-31 12:29:10 +0800

    Init Wechat Helper

--- app/code/BingHe/MeiJia/Controller/Index/Index.php
+++ app/code/BingHe/MeiJia/Controller/Index/Index.php
@@ -6,8 +6,20 @@ class Index extends \Magento\Framework\App\Action\Action
 {
     public function execute()
     {
+        $token = $this->_objectManager->get('BingHe\MeiJia\Helper\Wechat')->getToken();
+
+        var_dump($token);
+
+        /**
+        $object_manager = \Magento\Core\Model\ObjectManager::getInstance(); // \Magento\Framework\App\ObjectManager::getInstance();
+        $helper_factory = $object_manager->get('\Magento\Core\Model\Factory\Helper');
+        $helper = $helper_factory->get('\Magento\Core\Helper\Data');
+        */
+
+        /**
         $this->_view->loadLayout();
         $this->_view->getLayout()->initMessages();
         $this->_view->renderLayout();
+        */
     }
 }
---
+++ app/code/BingHe/MeiJia/Helper/Wechat.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Copyright © 2016 Ihor Vansach (ihor@magefan.com). All rights reserved.
+ * See LICENSE.txt for license details (http://opensource.org/licenses/osl-3.0.php).
+ *
+ * Glory to Ukraine! Glory to the heroes!
+ */
+
+namespace BingHe\MeiJia\Helper;
+
+/**
+ * Wechat Helper
+ */
+class Wechat extends \Magento\Framework\App\Helper\AbstractHelper
+{
+    /**
+     * Token
+     * 
+     * @return String
+     */
+    public function getToken()
+    {
+        return 'xx22x';
+    }
+}
